4d3ecae39baf8e2f22056932bb3e0b2f6bc9a862
Benjamin Nwokeleme <benwokeleme@gmail.com>
2020-03-09 01:27:13 -0400

    commit 03092020

---
+++ freecodecamp JS/learnjs.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+//alert("Your full name " + firstName + " " + lastName);
+//alert("Your age is " + age);
+console.log("Your full name " + firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.");
